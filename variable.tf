e172a87aa387722f47620a70e95c3780bc8f9200
RajniRani21 <rajnichaudhary762@gmail.com>
2022-03-16 19:40:24 +0530

    variable file commit

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "ami" {
+  type=string
+  description = "this value contains ami for instance"
+}
+
+variable "instance_type" {
+  type=string
+  description = "this variable holds the value of instance_type"
+}
+
+variable "name" {
+  type=string
+}
